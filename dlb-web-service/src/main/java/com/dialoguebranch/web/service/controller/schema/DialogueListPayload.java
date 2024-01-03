b15aa32fadcd1c873737dccd59c6215a92995bee
Harm op den Akker <hopdenakker@fruittreelabs.com>
2024-01-03 17:33:18 +0000

    DLB Web Service: Updated license info

--- dlb-web-service/src/main/java/com/dialoguebranch/web/service/controller/schema/DialogueListPayload.java
+++ dlb-web-service/src/main/java/com/dialoguebranch/web/service/controller/schema/DialogueListPayload.java
@@ -1,6 +1,6 @@
 /*
  *
- *                   Copyright (c) 2023 Fruit Tree Labs (www.fruittreelabs.com)
+ *                Copyright (c) 2023-2024 Fruit Tree Labs (www.fruittreelabs.com)
  *
  *
  *     This material is part of the DialogueBranch Platform, and is covered by the MIT License
@@ -8,7 +8,7 @@
  *
  *                                            ----------
  *
- * Copyright (c) 2023 Fruit Tree Labs (www.fruittreelabs.com)
+ * Copyright (c) 2023-2024 Fruit Tree Labs (www.fruittreelabs.com)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the "Software"), to deal in the Software without restriction,
