2e4f13f0e1aefe3ed2aa0693eeaa7d078b8c1cf7
Dennis Hofs <dennishofs@proton.me>
2025-04-21 21:57:13 +0200

    Added resizable panels to main page

--- dlb-web-client-vuejs/src/components/pages/MainPage.vue
+++ dlb-web-client-vuejs/src/components/pages/MainPage.vue
@@ -1,9 +1,31 @@
 <script setup>
-import { computed, inject } from 'vue';
+import { computed, inject, ref } from 'vue';
 import { DocumentFunctions } from '../../dlb-lib/util/DocumentFunctions.js';
 import HeaderMenuItem from '../widgets/HeaderMenuItem.vue';
 
 const state = inject('state');
+
+const dialogueBrowserWidth = ref(200);
+const variableBrowserWidth = ref(200);
+
+var dragResizeState;
+
+function initDragResize() {
+    dragResizeState = {
+        left: {
+            dragging: false,
+            startX: 0,
+            startWidth: 0,
+        },
+        right: {
+            dragging: false,
+            startX: 0,
+            startWidth: 0,
+        },
+    };
+}
+
+initDragResize();
 
 const versionInfo = computed(() => {
     return 'Not connected.';
@@ -14,6 +36,36 @@ function onLogoutClick() {
     DocumentFunctions.deleteCookie('user.authToken');
     DocumentFunctions.deleteCookie('user.role');
     state.value.user = null;
+}
+
+function onMouseDownLeftDivider(event) {
+    dragResizeState.left.dragging = true;
+    dragResizeState.left.startX = event.pageX;
+    dragResizeState.left.startWidth = dialogueBrowserWidth.value;
+}
+
+function onMouseDownRightDivider() {
+    dragResizeState.right.dragging = true;
+    dragResizeState.right.startX = event.pageX;
+    dragResizeState.right.startWidth = variableBrowserWidth.value;
+}
+
+function onMouseMove(event) {
+    if (dragResizeState.left.dragging) {
+        onDragLeftDivider(event);
+    } else if (dragResizeState.right.dragging) {
+        onDragRightDivider(event);
+    }
+}
+
+function onDragLeftDivider(event) {
+    dialogueBrowserWidth.value = dragResizeState.left.startWidth +
+        event.x - dragResizeState.left.startX;
+}
+
+function onDragRightDivider(event) {
+    variableBrowserWidth.value = dragResizeState.right.startWidth +
+        dragResizeState.right.startX - event.x;
 }
 </script>
 
@@ -29,7 +81,12 @@ function onLogoutClick() {
             </div>
         </header>
 
-        <main class="bg-background grow">
+        <main class="bg-background grow flex" @mousemove="onMouseMove" @mouseup="initDragResize" @mouseleave="initDragResize">
+            <div class="bg-white" :style="{width: dialogueBrowserWidth + 'px'}"></div>
+            <div class="w-2 cursor-col-resize" @mousedown="onMouseDownLeftDivider"></div>
+            <div class="grow bg-white"></div>
+            <div class="w-2 cursor-col-resize" @mousedown="onMouseDownRightDivider"></div>
+            <div class="bg-white" :style="{width: variableBrowserWidth + 'px'}"></div>
         </main>
     </div>
 </template>
